aba8870cf9b3f245ed8ba52ea4ca4e176d3710ab
Andy Wang <andywang0321@gmail.com>
2025-11-19 17:49:23 -0800

    added comments representing Sheeram's configs
    these are said to train better, notably `bidirectional = False`

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,29 +1,29 @@
-
-modelName = 'speechBaseline4'
+modelName = "speechBaseline4"  # Sheeram's configs
 
 args = {}
-args['outputDir'] = '/oak/stanford/groups/henderj/stfan/logs/speech_logs/' + modelName
-args['datasetPath'] = '/oak/stanford/groups/henderj/fwillett/speech/ptDecoder_ctc'
-args['seqLen'] = 150
-args['maxTimeSeriesLen'] = 1200
-args['batchSize'] = 64
-args['lrStart'] = 0.02
-args['lrEnd'] = 0.02
-args['nUnits'] = 1024
-args['nBatch'] = 10000 #3000
-args['nLayers'] = 5
-args['seed'] = 0
-args['nClasses'] = 40
-args['nInputFeatures'] = 256
-args['dropout'] = 0.4
-args['whiteNoiseSD'] = 0.8
-args['constantOffsetSD'] = 0.2
-args['gaussianSmoothWidth'] = 2.0
-args['strideLen'] = 4
-args['kernelLen'] = 32
-args['bidirectional'] = True
-args['l2_decay'] = 1e-5
+args["outputDir"] = "/Users/andywang/data/brain2speech/logs/speech_logs/" + modelName
+args["datasetPath"] = "/Users/andywang/data/brain2speech/ptDecoder_ctc"
+args["seqLen"] = 150
+args["maxTimeSeriesLen"] = 1200
+args["batchSize"] = 64  # 128
+args["lrStart"] = 0.02  # 0.05
+args["lrEnd"] = 0.02
+args["nUnits"] = 1024  # 256
+args["nBatch"] = 10000  # 3000
+args["nLayers"] = 5
+args["seed"] = 0
+args["nClasses"] = 40
+args["nInputFeatures"] = 256
+args["dropout"] = 0.4  # 0.2
+args["whiteNoiseSD"] = 0.8
+args["constantOffsetSD"] = 0.2
+args["gaussianSmoothWidth"] = 2.0
+args["strideLen"] = 4
+args["kernelLen"] = 32
+args["bidirectional"] = True  # False
+args["l2_decay"] = 1e-5
 
 from neural_decoder.neural_decoder_trainer import trainModel
 
 trainModel(args)
+
